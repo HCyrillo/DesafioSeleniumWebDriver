816bc0741deb632ac978a7592dc9e45b82b88f85
HCyrillo <giraweps.cyrillo@gmail.com>
2023-11-29 21:38:07 -0300

    Ajuste para execução

--- desafioSeleniumWebDriver/src/test/java/tests/PesquisaBlogTest.java
+++ desafioSeleniumWebDriver/src/test/java/tests/PesquisaBlogTest.java
@@ -6,11 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class PesquisaBlogTest {
@@ -21,7 +17,7 @@ public class PesquisaBlogTest {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\giraw\\AppData\\Local\\Programs\\Python\\Python312\\Scripts\\chromedriver.exe");
         driver.get("https://blogdoagi.com.br/");
         driver.manage().window().maximize();
-        WebDriver.Timeouts timeouts = driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
 
     }
@@ -44,7 +40,7 @@ public class PesquisaBlogTest {
         driver.findElement(By.className("search-submit")).click();
 
         String resultado = driver.findElement(By.className("entry-title")).getText();
-        Assert.assertEquals("Nenhum resultado", resultado)
+        Assert.assertEquals("Nenhum resultado", resultado);
     }
 
     @After
